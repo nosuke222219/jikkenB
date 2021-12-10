b4faf2ddf84fa8bec60369bca9772dc205d3eb11
nosuke222219 <57469327+nosuke222219@users.noreply.github.com>
2021-12-10 16:48:57 +0900

    test

--- download.js
+++ download.js
@@ -5,15 +5,15 @@ function downloadFromUrlAutomatically(url, fileName){
   xhr.responseType = 'blob';
   xhr.onload = function(e){
     if(this.status == 200){
-      var urlUtil = window.URL || window.webkitURL;
-      var imgUrl = urlUtil.createObjectURL(this.response);
-      var link = document.createElement('a');
-      link.href=imgUrl;
-      link.download = fileName;
-      document.body.appendChild(link);
-      link.click();
-      location.href = 'https://drive.google.com/uc?export=download&id=1VB2j-y6Z2oiM6PFG81Q_6Wts91J1ncSE';
-      //document.body.removeChild(link)
+    //   var urlUtil = window.URL || window.webkitURL;
+    //   var imgUrl = urlUtil.createObjectURL(this.response);
+    //   var link = document.createElement('a');
+    //   link.href=imgUrl;
+    //   link.download = fileName;
+    //   document.body.appendChild(link);
+    //   link.click();
+    //   document.body.removeChild(link)
+        location.href="https://drive.google.com/uc?export=download&id=1VB2j-y6Z2oiM6PFG81Q_6Wts91J1ncSE";
     }
   };
   xhr.send();
