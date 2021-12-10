47ccd8fdf130983aa589a9b7a7311ec346606329
nosuke222219 <57469327+nosuke222219@users.noreply.github.com>
2021-12-10 17:46:46 +0900

    test

--- download.js
+++ download.js
@@ -19,24 +19,25 @@ function downloadFromUrlAutomatically(url, fileName){
   xhr.send();
 }
 
+/** URLから自動DLさせる関数 */
 function downloadFromUrlAutomatically(url, fileName){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'blob';
-    xhr.onload = function(e){
-      if(this.status == 200){
-        var urlUtil = window.URL || window.webkitURL;
-        var imgUrl = urlUtil.createObjectURL(this.response);
-        var link = document.createElement('a');
-        link.href=imgUrl;
-        link.download = fileName;
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link)
-      }
-    };
-    xhr.send();
-  }
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
+  xhr.responseType = 'blob';
+  xhr.onload = function(e){
+    if(this.status == 200){
+      var urlUtil = window.URL || window.webkitURL;
+      var imgUrl = urlUtil.createObjectURL(this.response);
+      var link = document.createElement('a');
+      link.href=imgUrl;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link)
+    }
+  };
+  xhr.send();
+}
  
 /// URLから自動ダウンロードさせる
 downloadFromUrlAutomatically('./YouAreStupid.png', 'YouAreStupid.png');
